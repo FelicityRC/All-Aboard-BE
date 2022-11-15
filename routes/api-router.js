c80868cde5ee67f24c91632255a4c12822ec5da3
Joe <joefuller042@gmail.com>
2022-11-15 12:17:15 +0000

    rearrange router to be cleaner

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,14 +1,10 @@
 const apiRouter = require("express").Router();
 const userRouter = require("./users-router");
 const eventRouter = require("./events-router");
+const gameRouter = require("./games-router");
 
 apiRouter.use("/users", userRouter);
 apiRouter.use("/events", eventRouter);
-
-const gameRouter = require("./games-router");
-
-
 apiRouter.use("/games", gameRouter);
-
 
 module.exports = apiRouter;
