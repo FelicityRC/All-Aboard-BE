e17de7e387e9779d2319c81a8da014678d6ec33e
FelicityRC <felicitycarleton@hotmail.com>
2022-11-15 10:13:11 +0000

    add .env file, listen.js file and update scripts

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,16 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 
 const config =
-  ENV === 'production'
+  ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
         ssl: {
@@ -10,13 +18,5 @@ const config =
         },
       }
     : {};
-
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
 
 module.exports = new Pool(config);
