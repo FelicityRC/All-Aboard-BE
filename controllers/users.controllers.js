7416cfec8cd068dcb0a865185bfce72f7cb4e665
Joe <joefuller042@gmail.com>
2022-11-15 10:55:03 +0000

    add controllers folder and first controller

---
+++ controllers/users.controllers.js
@@ -0,0 +1,7 @@
+const { selectUsers } = require("../models/users.models");
+
+exports.getUsers = (req, res, next) => {
+  selectUsers().then((users) => {
+    res.status(200).send({ users });
+  });
+};
