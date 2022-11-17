47a29153ee697bfe05c9a8829055b271ad072796
Joe <joefuller042@gmail.com>
2022-11-17 10:29:53 +0000

    add GET /api/groups/:group_id/users endpoint, with testing and error handling

--- models/groups.models.js
+++ models/groups.models.js
@@ -25,3 +25,31 @@ exports.selectGroupByGroupId = (group_id) => {
       }
     });
 };
+
+exports.selectUsersByGroupId = (group_id) => {
+  // this checks the group_id is a positive integer
+  const num = Number(group_id);
+  if (!(Number.isInteger(num) && num > 0)) {
+    return Promise.reject({
+      status: 400,
+      msg: "group_id must be a positive integer",
+    });
+  }
+
+  return db
+    .query(`SELECT * FROM groups WHERE group_id=$1`, [group_id])
+    .then(({ rows: [group] }) => {
+      if (group) {
+        let queryString = `SELECT * FROM users WHERE `;
+        for (const user_id of group.users) {
+          queryString += `user_id=${user_id} OR `;
+        }
+        queryString = queryString.slice(0, -3);
+        return db.query(queryString).then(({ rows: users }) => {
+          return users;
+        });
+      } else {
+        return Promise.reject({ status: 404, msg: "Group Not Found" });
+      }
+    });
+};
--- routes/groups-router.js
+++ routes/groups-router.js
@@ -3,9 +3,11 @@ const groupRouter = require("express").Router();
 const {
   getGroups,
   getGroupByGroupId,
+  getUsersByGroupId,
 } = require("../controllers/groups.controllers");
 
 groupRouter.get("/", getGroups);
 groupRouter.get("/:group_id", getGroupByGroupId);
+groupRouter.get("/:group_id/users", getUsersByGroupId);
 
 module.exports = groupRouter;
